6b96282cced4bde70c0bc7967ee858d93456a3a5
KeigoHashimoto <kei.work.info@gmail.com>
2024-03-29 23:55:21 +0900

    最初のコミット

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+  import question from './question.json'; 
+  import { ref } from 'vue';
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  const ansFlg = ref(false);
+  const viewCorrect = () => {
+    ansFlg.value = true;
   }
-}
+ 
+  const qaNumber = ref(0);
+  const next = () => {
+    let num = Math.floor(Math.random() * question.qa.length);
+    if (num === qaNumber.value) {
+      num = Math.floor(Math.random() * question.qa.length);
+    }
+    qaNumber.value = num;
+    ansFlg.value = false;
+  }
+
 </script>
+
+<template>
+  <div>
+    <h1>応用技術者試験学習用APP</h1>
+    <!-- 問題ページ -->
+    <div>
+      <p>問題：{{ question.qa[qaNumber].q }}</p>
+      <p v-show="ansFlg">正解：{{ question.qa[qaNumber].a }}</p>
+      <button @click="viewCorrect()">正解を見る</button>
+    </div>
+    <!-- 次へボタン -->
+    <button @click="next()">次へ</button>
+  </div>
+</template>
 
 <style>
 #app {
