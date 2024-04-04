dc0f89cc6d3188d19328a6f3aabc7ee588cd991e
KeigoHashimoto <kei.work.info@gmail.com>
2024-04-05 01:27:05 +0900

    データの追加

--- src/App.vue
+++ src/App.vue
@@ -21,25 +21,54 @@
 
 <template>
   <div>
-    <h1>応用技術者試験学習用APP</h1>
     <!-- 問題ページ -->
-    <div>
-      <p>問題：{{ question.qa[qaNumber].q }}</p>
-      <p v-show="ansFlg">正解：{{ question.qa[qaNumber].a }}</p>
+    <div class="inner-width">
+      <h1>応用技術者試験学習用APP</h1>
+      <div class="q">
+        <p>問題：{{ question.qa[qaNumber].q }}</p>
+      </div>
+      <div class="a">
+        <p v-show="ansFlg">正解：{{ question.qa[qaNumber].a }}</p>
+      </div>
       <button @click="viewCorrect()">正解を見る</button>
+      
+      <!-- 次へボタン -->
+      <button @click="next()">次へ</button>
     </div>
-    <!-- 次へボタン -->
-    <button @click="next()">次へ</button>
+
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.inner-width {
+  width:350px;
+  margin: 0 auto;
+}
+
+.q {
+  border: 2px solid #ccc;
+  border-radius: 5px;
+  padding:30px;
+  margin: 1rem 0;
+  font-size: 1.4rem;
+  line-height: 1.7;
+
+}
+
+.a {
+  background: #c8e3ff;
+  padding: 30px;
+  border-radius: 5px;
+  font-size: 1.4rem;
+  line-height: 1.7;
+}
+
+button {
+  background: rgb(118, 216, 118);
+  border: none;
+  border-radius: 3px;
+  padding: 15px 25px;
+  margin-right: 5px;
+  margin-top: 10px;
 }
 </style>
